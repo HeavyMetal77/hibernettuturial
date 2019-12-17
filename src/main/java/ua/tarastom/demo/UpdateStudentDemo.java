35d58d12be62f701af94cc736084f5aaddc94098
HeavyMetal77 <ttm2006@ukr.net>
2019-12-17 12:15:28 +0200

    Updating Objects with Hibernate

---
+++ src/main/java/ua/tarastom/demo/UpdateStudentDemo.java
@@ -0,0 +1,46 @@
+package ua.tarastom.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import ua.tarastom.entity.Student;
+
+
+public class UpdateStudentDemo {
+    public static void main(String[] args) {
+
+        SessionFactory sessionFactory = new Configuration().configure("hibernate.cfg.xml")
+                                        .addAnnotatedClass(Student.class).buildSessionFactory();
+
+        Session currentSession = sessionFactory.getCurrentSession();
+
+        try{
+            int studentId = 1;
+            currentSession.beginTransaction();
+            Student student = currentSession.get(Student.class, studentId);
+            System.out.println(student);
+            System.out.println("Updating student....");
+            //имя обновляется только в коде
+            student.setFirstName("Bob");
+
+            //имя обновляется также и в базе после коммита
+            currentSession.getTransaction().commit();
+
+            // NEW CODE
+            currentSession = sessionFactory.getCurrentSession();
+            currentSession.beginTransaction();
+
+            // update email for all students
+            System.out.println("Update email for all students");
+            currentSession.createQuery("update Student set email='foo@gmail.com'").executeUpdate();
+
+            // commit the transaction
+            currentSession.getTransaction().commit();
+            System.out.println("Done!");
+        }finally {
+            sessionFactory.close();
+        }
+
+
+    }
+}
